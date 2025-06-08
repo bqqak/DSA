72f858ea0ed525f21b9bc5a6faf08fccc4fd08a8
bqqak <110017196+bqqak@users.noreply.github.com>
2025-06-09 01:50:16 +0500

    Time: 0 ms (100%), Space: 8.3 MB (96.47%) - LeetHub

--- 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.cpp
+++ 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.cpp
@@ -1,9 +1,14 @@
 class Solution {
 public:
     int differenceOfSums(int n, int m) {
-        int num1 = 0, num2 = 0;
-        for(int i = m; i <= n; i+=m) num2 += i;
-        for(int i = 1; i <= n; i++) num1 += (i % m == 0 ? 0 : i);
-        return num1 - num2;
+        // using maths, because maths is a power
+        //using sum formula   (1, n) -> S = (1 + n) * n / 2;
+        // S = num1 + num2,   so   num1 = S - num2,   we need find num1 - num2 = S - 2num2
+        // k = n / m (number that divisible by m). For instance n = 17, m = 3 -> ans: 5   (3, 6, 9, 12, 15)
+        // num2 = m + 2m + 3m + 4m + km = m(1 + 2 + 3 + 4 + k) = m * k(1+k) / 2
+        int S = (1 + n) * n / 2;
+        int k = n / m;
+        int num2 = m * k * (1 + k) / 2;
+        return S - 2 * num2;
     }
 };
