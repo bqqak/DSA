3d919237fef59b8a88d2c2ed9a212eee01c2d2ff
bqqak <110017196+bqqak@users.noreply.github.com>
2025-06-09 02:06:04 +0500

    Update 3110-score-of-a-string.cpp

--- 3110-score-of-a-string/3110-score-of-a-string.cpp
+++ 3110-score-of-a-string/3110-score-of-a-string.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int scoreOfString(string s) {
-        // s.length <= 100, thus we can solve it in O(n^3)
+        
         int cnt = 0;
         for(int i = 0; i < s.size() - 1; i++){
             cnt += abs(int(s[i]) - int(s[i + 1]));
