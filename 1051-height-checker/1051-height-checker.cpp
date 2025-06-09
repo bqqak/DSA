1c2d72937d0285a094bd1c7388c6beb22471df01
bqqak <110017196+bqqak@users.noreply.github.com>
2025-06-09 17:11:57 +0500

    Time: 0 ms (100%), Space: 11.4 MB (55.95%) - LeetHub

--- 1051-height-checker/1051-height-checker.cpp
+++ 1051-height-checker/1051-height-checker.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
     int heightChecker(vector<int>& heights) {
-        // Insertion Sort
-        vector<int> copy = heights;
-        for(int i = 1; i < copy.size(); i++){
-            for(int j = i; j > 0; j--){
-                if(copy[j] < copy[j - 1]){
-                    swap(copy[j], copy[j - 1]);
-                } else break;
-            }
+        //  Counting Sort
+        int mx = 0;
+        for(int i : heights) mx = max(mx, i);
+        
+        vector<int> v(mx + 1, 0);
+        for(int i : heights) v[i]++;
+
+        for(int i = 1; i <= mx; i++) v[i] += v[i - 1];
+
+        int n = heights.size();
+        vector<int>ans(n);
+        for(int i = n - 1; i >= 0; i--){
+            int x = heights[i];
+            int pos = --v[x];
+            ans[pos] = x;
         }
         int cnt = 0;
-        for(int i = 0; i < heights.size(); i++){
-            if(heights[i] != copy[i]) cnt++;
+        for(int i = 0; i < n; i++){
+            if(heights[i] != ans[i]) cnt++;
         }
         return cnt;
     }
