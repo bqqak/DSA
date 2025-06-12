fd34380bb0773b79b8e09aba8828f6857ea35361
bqqak <110017196+bqqak@users.noreply.github.com>
2025-06-12 14:06:37 +0500

    Time: 27 ms (6.23%), Space: 15.1 MB (97.04%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
-    static bool compare(int a, int b){
-        return to_string(a) + to_string(b) > to_string(b) + to_string(a);
-    }
     string largestNumber(vector<int>& nums) {
-        sort(nums.begin(), nums.end(), compare);
+        int n = nums.size();
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n - i - 1; j++){
+                string a = to_string(nums[j]);
+                string b = to_string(nums[j + 1]);
+                if(a + b < b + a){
+                    swap(nums[j], nums[j + 1]);
+                }
+            }
+        }
         string res = "";
-        for(int i : nums) res += to_string(i);
-        
-        return (res[0] == '0' ? "0" : res);
+        for(int i = 0; i < n; i++){
+            res += to_string(nums[i]);
+        }
+        if(res[0] == '0') return "0";
+        return res;
     }
 };
