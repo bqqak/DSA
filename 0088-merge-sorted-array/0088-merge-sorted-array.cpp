36475b07e6a735d370d7359bd6a91a6c2d691fb6
bqqak <110017196+bqqak@users.noreply.github.com>
2025-06-12 20:09:14 +0500

    Time: 7 ms (1.37%), Space: 12.2 MB (95.72%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -6,13 +6,12 @@ public:
           int j = n - 1;
           int k = m + n - 1;
           while(i >= 0 && j >= 0){
-            if(nums1[i] < nums2[j]){
-                nums1[k] = nums2[j];
-                j--;
+            if(nums1[i] > nums2[j]){
+                nums1[k] = nums1[i];
+                i--;
             } else{
                 nums1[k] = nums2[j];
-                swap(nums1[k], nums1[i]);
-                i--;
+                j--;
             }
             k--;
           }
