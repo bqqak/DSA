165c3c24fbeacd86ae124fd1311b1ab3ac4fa191
bqqak <110017196+bqqak@users.noreply.github.com>
2025-06-13 22:56:57 +0500

    Time: 0 ms (100%), Space: 17.9 MB (32.93%) - LeetHub

--- 3467-transform-array-by-parity/3467-transform-array-by-parity.py
+++ 3467-transform-array-by-parity/3467-transform-array-by-parity.py
@@ -1,13 +1,11 @@
 class Solution:
     def transformArray(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        for i in range(n):
-            nums[i] = 0 if nums[i] % 2 == 0 else 1
-        # Realize selection sort
-        for i in range(n):
-            mn_idx = i
-            for j in range(i + 1, n):
-                if nums[mn_idx] > nums[j]:
-                    mn_idx = j
-            nums[mn_idx], nums[i] = nums[i], nums[mn_idx]
-        return nums
+        # the idea is to push 0 to the begin, and 1 to the end
+        arr = []
+        for num in nums:
+            if num % 2 == 0:
+                arr.insert(0, 0)
+            else:
+                arr.append(1)
+        return arr
