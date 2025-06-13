560a514cdc3241096bf8e5147e158043fc35b164
bqqak <110017196+bqqak@users.noreply.github.com>
2025-06-13 23:13:15 +0500

    Time: 65 ms (13.29%), Space: 18.2 MB (20.57%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.py
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -3,14 +3,9 @@ class Solution:
         # [2, 5, 1, 3, 4, 7]   n = 3
         # x: 2(0) 5(1) 1(2)   y: 3(3)  4(4) 7(5)
         arr = []
-        l = 0
-        r = n
-        while l != n:
-            arr.append(nums[l])
-            arr.append(nums[r])
-            l+=1
-            r+=1
-        # l = 0  r = 3
-        #  0 != 2  ->    [2, 3]   l = 1  r = 4
-        #  1 != 2 ->   [2, 3, 5, 4]  l = 2 r = 5
+        for i in range(n):
+            arr.append(nums[i])
+            arr.append(nums[n + i])
+        # i = 0  [2, 3]
+        # i = 1  [2, 3, 5, 4]
         return arr
