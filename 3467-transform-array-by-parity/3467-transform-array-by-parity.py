6fc9bc5e6a795391107339a76329322a3645ea4d
bqqak <110017196+bqqak@users.noreply.github.com>
2025-06-13 22:49:26 +0500

    Time: 8 ms (3.52%), Space: 18 MB (11.59%) - LeetHub

--- 3467-transform-array-by-parity/3467-transform-array-by-parity.py
+++ 3467-transform-array-by-parity/3467-transform-array-by-parity.py
@@ -1,6 +1,13 @@
 class Solution:
     def transformArray(self, nums: List[int]) -> List[int]:
-        for i in range(len(nums)):
+        n = len(nums)
+        for i in range(n):
             nums[i] = 0 if nums[i] % 2 == 0 else 1
-        nums.sort()
+        # Realize selection sort
+        for i in range(n):
+            mn_idx = i
+            for j in range(i + 1, n):
+                if nums[mn_idx] > nums[j]:
+                    mn_idx = j
+            nums[mn_idx], nums[i] = nums[i], nums[mn_idx]
         return nums
