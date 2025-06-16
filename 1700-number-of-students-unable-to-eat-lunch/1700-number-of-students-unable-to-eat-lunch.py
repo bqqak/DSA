0da022f51e18fe04344b68d54b33ea9b4b5338ec
bqqak <110017196+bqqak@users.noreply.github.com>
2025-06-16 22:13:15 +0500

    Time: 0 ms (100%), Space: 17.7 MB (62.38%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
@@ -1,16 +1,19 @@
 class Solution:
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
-        # based on examples, if students[0] == sandwiches[0], then pop it, otherwise  to the end append
-        
-        while len(students) != 0:
-            if students[0] == sandwiches[0]:
-                students.pop(0)
-                sandwiches.pop(0)
+        square = 0
+        for num in students:
+            square += num
+        circular = len(students) - square
+
+        for i in sandwiches:
+            if i == 0:
+                if circular == 0:
+                    return square
+                else:
+                    circular -= 1
             else:
-                students.append(students.pop(0))
-            if 1 in students and 0 not in students and sandwiches[0] == 0:
-                break
-            elif 0 in students and 1 not in students and sandwiches[0] == 1:
-                break
-        return len(students)
-            
+                if square == 0:
+                    return circular
+                else:
+                    square -= 1
+        return 0
