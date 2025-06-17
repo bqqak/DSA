adacaf28f2342f33675771421478b344f1c490d1
bqqak <110017196+bqqak@users.noreply.github.com>
2025-06-18 01:07:53 +0500

    Time: 7 ms (97.37%), Space: 17.9 MB (90.71%) - LeetHub

--- 2535-difference-between-element-sum-and-digit-sum-of-an-array/2535-difference-between-element-sum-and-digit-sum-of-an-array.py
+++ 2535-difference-between-element-sum-and-digit-sum-of-an-array/2535-difference-between-element-sum-and-digit-sum-of-an-array.py
@@ -3,7 +3,7 @@ class Solution:
         sm = sum(nums)
         sm_digit = 0
         for num in nums:
-            while num != 0:
+            while num > 0:
                 last_digit = num % 10
                 sm_digit += last_digit
                 num //= 10
