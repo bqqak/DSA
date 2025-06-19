564b84f89a50a5aab07e34dd2d6a9cee9d09013b
bqqak <110017196+bqqak@users.noreply.github.com>
2025-06-19 12:36:57 +0500

    Time: 0 ms (100%), Space: 17.8 MB (37.62%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
@@ -1,19 +1,19 @@
 class Solution:
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        # either eats square or circular sandwiches
         square = 0
-        for num in students:
-            square += num
+        for i in students:
+            square += i
         circular = len(students) - square
-
         for i in sandwiches:
-            if i == 0:
-                if circular == 0:
-                    return square
-                else:
-                    circular -= 1
-            else:
+            if i == 1:
                 if square == 0:
                     return circular
                 else:
                     square -= 1
-        return 0
+            else:
+                if circular == 0:
+                    return square
+                else:
+                    circular -= 1
+        return square + circular
