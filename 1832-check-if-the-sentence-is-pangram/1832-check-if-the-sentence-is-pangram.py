07e6c624e6cf8dd57adf59ffc694d013382b2122
bqqak <110017196+bqqak@users.noreply.github.com>
2025-06-25 21:31:26 +0500

    Time: 0 ms (100%), Space: 17.7 MB (80.37%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
@@ -3,5 +3,5 @@ class Solution:
         alphabet = [chr(i) for i in range(97, 123)]
         for i in alphabet:
             if sentence.find(i) == -1:
-                return false
-        return true
+                return False
+        return True
