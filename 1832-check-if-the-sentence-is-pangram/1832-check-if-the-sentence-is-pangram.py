49481b170e8807709afe2b0b2e1d015c197e91e4
bqqak <110017196+bqqak@users.noreply.github.com>
2025-06-25 21:31:11 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
@@ -0,0 +1,7 @@
+class Solution:
+    def checkIfPangram(self, sentence: str) -> bool:
+        alphabet = [chr(i) for i in range(97, 123)]
+        for i in alphabet:
+            if sentence.find(i) == -1:
+                return false
+        return true
