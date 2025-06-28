ab9f69e813f8cdc06902c7f59bba831577a97f94
bqqak <110017196+bqqak@users.noreply.github.com>
2025-06-29 00:09:44 +0500

    Time: 50 ms (5.3%), Space: 17.6 MB (73.64%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -0,0 +1,24 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        '''
+            Using two-pointers. i = 0 -> for first string
+                                j = 0 -> for second string
+        '''
+        ans = ""
+        i = 0
+        j = 0
+        n = len(word1)
+        m = len(word2)
+        while i < n and j < m:
+            ans += word1[i]
+            ans += word2[j]
+            i += 1
+            j += 1
+        
+        while i < n:
+            ans += word1[i]
+            i+=1
+        while j < m:
+            ans += word2[j]
+            j+=1
+        return ans
