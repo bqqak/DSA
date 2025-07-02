cdcb27ae22e31f3aa50ab0eae91c06e4f6091798
bqqak <110017196+bqqak@users.noreply.github.com>
2025-07-02 19:08:31 +0500

    Time: 153 ms (80.74%), Space: 31.1 MB (70.83%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,15 +1,24 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        '''
-        the first easy way
-        O(n * k)
-        '''
-        mx = []
-        for i in range(len(nums) - k + 1):
-            maxi = nums[i]
-            for j in range(i, i + k):
-                maxi = max(maxi, nums[j])
-            mx.append(maxi)
-        return mx
+        dec_stack = deque()
+        n = len(nums)
+        output = [0] * (n - k + 1)
+        
+        for i in range(k):
+            while dec_stack and nums[i] > nums[dec_stack[-1]]:
+                dec_stack.pop()   
+            dec_stack.append(i)
 
-     
+        output[0] = nums[dec_stack[0]]
+
+        for i in range(k, n):
+            if dec_stack and dec_stack[0] <= i - k:
+                dec_stack.popleft()
+            
+            while dec_stack and nums[i] > nums[dec_stack[-1]]:
+                dec_stack.pop()
+            dec_stack.append(i)
+
+            output[i - k + 1] = nums[dec_stack[0]]
+        
+        return output
