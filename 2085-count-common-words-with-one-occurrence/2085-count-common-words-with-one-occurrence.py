11d5ce97e7bc4422d6a0293c105af974a2e9fa6f
bqqak <110017196+bqqak@users.noreply.github.com>
2025-07-03 21:42:57 +0500

    Time: 3 ms (72.17%), Space: 18.5 MB (7.16%) - LeetHub

--- 2085-count-common-words-with-one-occurrence/2085-count-common-words-with-one-occurrence.py
+++ 2085-count-common-words-with-one-occurrence/2085-count-common-words-with-one-occurrence.py
@@ -1,17 +1,6 @@
 class Solution:
     def countWords(self, words1: List[str], words2: List[str]) -> int:
-        mp_1 = {}
-        mp_2 = {}
-        for word in words1:
-            mp_1[word] = mp_1.get(word, 0) + 1
-        
-        for word in words2:
-            mp_2[word] = mp_2.get(word, 0) + 1
-        
-        cnt = 0
+        mp_1 = Counter(words1)
+        mp_2 = Counter(words2)
 
-        for key in mp_1:
-            if mp_1[key] == 1:
-                if mp_2.get(key) == 1:
-                    cnt += 1
-        return cnt
+        return sum(mp_1[key] == mp_2[key] == 1 for key in mp_1)
